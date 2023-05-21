f8ac054b94fef29a6349d5ef94757b849b9fc489
bryan.perez193 <bryan.perez193@tectijuana.edu.mx>
2023-05-21 14:48:01 -0700

    La neta no sé qué pasa ayudaaaa

--- Pantallas/MostrarCarro.js
+++ Pantallas/MostrarCarro.js
@@ -1,45 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native'
-import firebase from './firebase';
+import firebase from '../Firebase'
 
-const MostrarCarro = ({carroId}) => {
-    const [Carro, setCarro] = useState(null);
-    useEffect(() => {
-        const carroRef = firebase.database().ref(`Carros/${carroId}`);
-        carroRef.on('value', (snapshot) => {
-          setCarro(snapshot.val());
-        });
+const MostrarCarro = ({ carroId }) => {
+  const [Carro, setCarro] = useState(null);
 
-        return () => {
-            carroRef.off();
-          };
-        }, [carroId]);
-      
-        if (!Carro) {
-          return null;
-        }
-      
-        return (
-          <View>
-            <Text>{MostrarCarros.Altura} M</Text>
-            <Text>{MostrarCarros.Ancho} M</Text>
-            <Text>{MostrarCarros.Colores}</Text>
-            <Text>{MostrarCarros.Longitud} M</Text>
-            <Text>{MostrarCarros.Marca}</Text>
-            <Text>{MostrarCarros.carroId}</Text>
-            <Text>{MostrarCarros.Peso} KG</Text>
-          </View>
-        );
-      };
+  useEffect(() => {
+    const carroRef = firebase.database().ref(`Carros/${carroId}`);
+    carroRef.on('value', (snapshot) => {
+      setCarro(snapshot.val());
+    });
 
-export default MostrarCarro
+    return () => {
+      carroRef.off();
+    };
+  }, [carroId]);
+
+  if (!Carro) {
+    return null;
+  }
+
+  return (
+    <View>
+      <Text>Hola</Text>
+      <Text>{carroId}</Text>
+      <Text>{Carro.Altura} M</Text>
+      <Text>{Carro.Ancho} M</Text>
+      <Text>{Carro.Colores}</Text>
+      <Text>{Carro.Longitud} M</Text>
+      <Text>{Carro.Marca}</Text>
+      <Text>{Carro.Peso} KG</Text>
+    </View>
+  );
+};
+
+export default MostrarCarro;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'space-around',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
 });
-  
